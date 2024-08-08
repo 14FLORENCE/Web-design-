1019b1164b87a0ed7d3affdf629dcd45ccdd77ab
SWAY <www.florencesway@gmail.com>
2024-08-08 11:45:14 +0300

    changes

--- storefront/playground/apps.py
+++ storefront/playground/apps.py
@@ -3,4 +3,5 @@ from django.apps import AppConfig
 
 class PlaygroundConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'playground'
+    name = 'full.python.path.to.your.app.playground'
+    label='my.playground'
